f45a2791f09df1125e2afcc3266c4622a11b8ad5
terencelim <terence.lim@gmail.com>
2016-10-19 16:41:06 +1100

    prelaunch streamlining

--- js/grayscale.js
+++
@@ -1,6 +0,0 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-$(window).scroll(function(){$(".navbar").offset().top>250?$(".brand-logo").css("visibility","visible"):$(".brand-logo").css("visibility","hidden")}),$(function(){$("a.page-scroll").bind("click",function(o){var i=$(this);$("html, body").stop().animate({scrollTop:$(i.attr("href")).offset().top},1500,"easeInOutExpo"),o.preventDefault()})}),$(".navbar-collapse ul li a").click(function(){$(".navbar-toggle:visible").click()}),$("a").mouseup(function(){$(this).blur()}),$("body").scrollspy({target:".navbar-fixed-top"}),$("div.modal").on("show.bs.modal",function(){var o=this,i=o.id;window.location.hash=i,window.onhashchange=function(){location.hash||$(o).modal("hide")}});
